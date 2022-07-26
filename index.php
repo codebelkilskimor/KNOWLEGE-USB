c27bfe81c841baca4b933cb47e468c70f9f7c55a
codebelkilskimor <110052962+codebelkilskimor@users.noreply.github.com>
2022-07-26 11:59:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<link rel="icon" href="img/loggo.ico">
+<header>
+		<link rel="stylesheet" href="css/estilos.css">	
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <head>
+    <title>Ingreso KNOWLEGE USB</title> 
+</head>   
+</header> 	
+	
+		<body>
+			<section id="banner">
+				<!-- Logo de la universidad-->
+				<img src="img/logousb.png">
+				
+			</section>
+			<br>
+			<!-- Texto introducción-->
+			<section id="bienvenidos">
+            <div class="contenedor-c">
+                <h2>KNOWLEGE USB - MAPA DE CONOCIMIENTO <br> UNIVERSIDAD SAN BUENAVENTURA </h2><br></br>
+               
+                </div>
+            </section>
+			<!-- Login-->
+		               <section id="conten">
+			               <form>
+				               <div id="numcc">
+				               <h2 class="numcc"></h2>
+				         <div>
+				               <input class="casillauno" pattern="[A-Za-z0-9_-.@]{1,50}" required placeholder="Usuario" id="email" type="Texto" ="fade" name="Email_txt" requiered />
+                                       </div>
+
+                                       <div>        
+                               <input class="casillados" pattern="[A-Za-z0-9_-]{1,10}" required placeholder="Contraseña" id="asunto" type="pasword" ="fade" name="Pass_txt" requiered />
+                                       </div>
+				               <button class="button-i" type="submit">INGRESAR</button>
+
+				<!-- Login<select class="opcion" name="rol">
+					<option disabled="disabled" selected="selected"> --- Seleccione Su Rol</option>
+					<option>Invitado</option>
+					<option>Director de investigaciones</option>
+					<option>Director de grupo de investigación</option>
+					<option>Coordinador de investigaciones de facultad</option>
+					<option>Director de trabajo de grado</option>
+					<option>Docente investigador</option>
+					<option>Investigador en formación</option>
+					<option>Biblioteca</option>
+					<option>Dirección de tecnología de la universidad</option>-->
+					</select>
+				
+            <div class="txtdow">	
+					
+                <p>Querido usuario acá podrá realizar la gestión del conocmiento de la universidad 
+                San Buenaventura bogotá, ingrese sus datos y posteriormente acepte los terminos de seguridad 
+            </div>
+				
+
+			</form>
+			</section>
+			</div>
+			<!-- End Login-->
+			<br>
+
+
+
+		</body>
+		<footer>
+	
+		</footer>
+</html>  
---
+++ politica.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<link rel="icon" href="img/loggo.ico">
+<header>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link rel="stylesheet" href="css/estilos.css">
+	    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald" rel="stylesheet">
+		<link rel="stylesheet" href="css/estilos.css">	
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!DOCTYPE html>
+</header>
+<html lang="en">
+<head>
+	<title>Politicas de protección de datos KNOWLEGE USB</title> 
+</head>
+<body>
+	<section id="bienvenidos">
+
+            <div class="contenedor-d">
+                <h2>KNOWLEGE USB <br> POLITICA DE TRATAMIENTO DE DATOS</h2><br></br>
+                </div>
+            </section>
+
+	<div class="contenedor">
+		
+		<h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti consectetur minus deleniti culpa ducimus alias blanditiis, suscipit eligendi vel quam magni ad quasi iure? Nulla dolorum repellat atque laboriosam. Vel?</h5>
+
+		<h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sequi provident expedita esse, nostrum corporis reiciendis, vel odio laboriosam dolorum distinctio totam, soluta delectus dolores vero. Eum libero laborum sunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sequi provident expedita esse, nostrum corporis reiciendis, vel odio laboriosam dolorum distinctio totam, soluta delectus dolores vero. Eum libero laborum sunt.</h5>
+
+		<h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sequi provident expedita esse, nostrum corporis reiciendis, vel odio laboriosam dolorum distinctio totam, soluta delectus dolores vero. Eum libero laborum sunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sequi provident expedita esse, nostrum corporis reiciendis, vel odio laboriosam dolorum distinctio totam, soluta delectus dolores vero. Eum libero laborum sunt.</h5>
+	</div>
+	<button class="button-a" type="submit" >RECHAZAR</button>
+	<button class="button-b" type="submit" >ACEPTAR</button>
+</body>
+		<footer>
+	
+		</footer>
+</html>
